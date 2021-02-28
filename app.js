9e94ed6d30829e9e936cfce9d3613d64e74b5ac3
szarlata <e.a.szarlata@gmail.com>
2021-02-28 20:39:36 +0100

    MongoDB and mongoose - getting and saving data

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog')
+
+
 
 // express app
 const app = express();
 
-// register view engine
+//connect to mongoDB
+const dbURI = 'mongodb+srv://Ewa:test123@cluster0.oincz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+ .then((result) => app.listen(3000))
+ .catch((err) => console.log(err))
+
+ // register view engine
 app.set('view engine', 'ejs');
 
-app.listen(3000);
+
+
+//this middleware allows the browser to access files in the public folder
+app.use(express.static('public'));
 
 // third party middleware: Morgan
 app.use(morgan('dev'));
@@ -21,28 +34,63 @@ app.use(morgan('dev'));
         next();
     }); */
 
+//mongose and mongo sandbox routes
+/* app.get('/add-blog', (req, res) => {
+ const blog = new Blog({
+     title: "new blog",
+     snippet: "about my new blog",
+     body: "more about my new blog"
+ });
+ blog.save()
+ .then((result) => {
+     res.send(result)
+ })
+ .catch((err) => {
+     console.log(err)
+ });
+});
 
+app.get('/all-blogs', (req, res) => {
+   Blog.find()
+   .then((result) => {
+       res.send(result);
+   })
+   .catch((err) => {
+       console.log(err)
+   }); 
+});
+
+app.get('/single-blog', (req, res) => {
+    Blog.findById('603be9e1b739d4ee35695861')
+    .then((result) => {
+        res.send(result)
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+})
+ */
+//end of block
 
 app.get('/', (req, res) => {
-const blogs = [
-    {title: "Ewa's 1st blog", snippet: "Lorem ipsum"},
-    {title: "Ewa's 2nd blog", snippet: "Lorem ipsum"},
-    {title: "Ewa's 3rd blog", snippet: "Lorem ipsum"}
-];
-
-
-    //sending html
-    //res.send('<p>Express here, hello</p>');
-res.render('index', { title: 'Home', blogs});
-    /* //sending file
-    res.sendFile('./views/index.html', { root: __dirname });
-    // object { root: __dirname } - we need absolute path to the file or definition of root directory */
+ res.redirect('/blogs');
 });
 
 
 app.get('/about', (req, res) => {
     res.render('about', { title: 'About'});
 });
+
+//blog routes
+app.get('/blogs', (req, res) => {
+    Blog.find().sort({ createdAt: -1 })
+    .then((result) => {
+res.render('index', { title: 'All blogs', blogs: result })
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+})
 
 app.get('/blogs/create', (req, res) => {
     res.render('create', { title: 'Create'});
---
+++ models/blog.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const blogSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    snippet: {
+      type: String,
+      required: true,
+    },
+    body: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+// the name of this module is important - it will be pluralized and the database will be search for this name 
+const blog = mongoose.model('Blog', blogSchema);
+module.exports = blog;
